82c40e2ae48e9ddbd5e5d41cf9f48bc200ef8a2e
pc <mohamedsafan2468@gmail.com>
2022-10-17 23:53:33 +0200

    version5

--- app/src/main/java/com/graduationproject/robokidsapp/ui/MainActivity.kt
+++ app/src/main/java/com/graduationproject/robokidsapp/ui/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        // بسم الله
-        // mohamed rafat
+        // بسنم الله الرحمن الرحيم
     }
 }
