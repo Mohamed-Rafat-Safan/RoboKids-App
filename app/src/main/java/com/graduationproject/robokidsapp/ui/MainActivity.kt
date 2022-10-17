e87561065da20af4342a1c0df1a63d3dd23fe7c6
mohamed samir <smyr03130@gmailcom>
2022-10-17 23:42:59 +0200

    commit

--- app/src/main/java/com/graduationproject/robokidsapp/ui/MainActivity.kt
+++ app/src/main/java/com/graduationproject/robokidsapp/ui/MainActivity.kt
@@ -16,5 +16,6 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         Toast.makeText(this, "mohamed", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "mohamed", Toast.LENGTH_SHORT).show()
     }
 }
